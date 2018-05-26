3af031d513633bf51a2f0695a953a2970a0d27c0
m3rlin94 <naveen.cooray.nc@gmail.com>
2018-05-26 22:56:15 +0530

    Registration java firebase code add

--- app/src/main/java/com/creativitude/jeewa/Register.java
+++ app/src/main/java/com/creativitude/jeewa/Register.java
@@ -2,8 +2,10 @@ package com.creativitude.jeewa;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -12,9 +14,15 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.creativitude.jeewa.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
 
+    private static final String TAG = "RegisterActivity";
     private EditText name;
     private EditText age;
     private EditText contact_no;
@@ -31,6 +39,9 @@ public class Register extends AppCompatActivity {
 
     private User user;
     private int error;
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,12 +65,35 @@ public class Register extends AppCompatActivity {
         confirm_password = findViewById(R.id.et_reg_confirm_password);
 
         user = new User();
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
 
     public void register(View view) {
         error = 0;
         validateInputs();
+
+        mAuth.createUserWithEmailAndPassword(user.getEmail(), user.getPassword())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(Register.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
 
     }
 
